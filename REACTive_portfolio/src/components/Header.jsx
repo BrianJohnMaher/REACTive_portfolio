b048ca502a7d8cfecc5f1ce04eaf9d106d27c1de
BRIAN MAHER <bmaher22@mac.comclear>
2023-11-17 07:27:48 -0500

    first commit

---
+++ REACTive_portfolio/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Navigation from "./Navigation";
+
+
+export default function Header(){
+    return(
+        <header>
+            <h1>Brian maher...</h1>
+            <Navigation/>
+        </header>
+    )
+}
